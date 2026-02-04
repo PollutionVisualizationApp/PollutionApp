c568c54422c80ba99b169dc51d3154aca11f93f0
Stefan Velkov <velkov29stefan@gmail.com>
2026-02-04 10:34:26 +0100

    Sensors adder, daily mesurments added, changed filter logic and added ffill on missing data

---
+++ scripts/dailyClass.js
@@ -0,0 +1,219 @@
+class DailyData{
+    constructor(apex){
+        this.chart = apex;
+        this.sensorData =[];
+        this.initChart()
+    }
+
+    formatWithTimezone(date) {
+
+    const offset = date.getTimezoneOffset();
+    const sign = offset <= 0 ? '+' : '-';
+    const hours = String(Math.abs(Math.floor(offset / 60))).padStart(2, '0');
+    const minutes = String(Math.abs(offset % 60)).padStart(2, '0');
+    
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hour = String(date.getHours()).padStart(2, '0');
+    
+    return `${year}-${month}-${day}T${hour}:00:00${sign}${hours}:${minutes}`;
+
+    }
+
+    getDates(){
+            const now = new Date();
+            now.setHours(now.getHours() - 1);
+            now.setMinutes(0, 0, 0);
+
+            // 24 hours before
+            const yesterday = new Date(now.getTime() - 25 * 60 * 60 * 1000);
+        
+            const urlEncoded = encodeURIComponent(this.formatWithTimezone(now));
+            const yesterdayEncoded = encodeURIComponent(this.formatWithTimezone(yesterday));
+
+            return {
+                now: urlEncoded,
+                yesterday: yesterdayEncoded
+            }
+
+    }
+    getTimeOnly(date){
+        const time = date.split('T')[1].slice(0, 5);
+        return time;
+    }
+
+    hourDiff(prev, curr){
+        const start = new Date(prev);
+        const end = new Date(curr);
+
+        const diffMs = end - start;
+        const diffHours = diffMs / (1000 * 60 * 60);
+
+        // console.log(diffHours);
+        return diffHours;
+    }
+
+    fillGaps(arr){
+        let previousHour=null;
+        let newArr=[];
+
+
+        arr.forEach(a=>{
+            if(previousHour==null){
+                previousHour = a.index;
+            }
+            if(this.hourDiff(previousHour,a.index)>1){
+                //  console.log(previousHour+" "+a.index);
+                // console.log(a);
+                const [baseHour] = previousHour.split('T')[1].split(':').map(Number);
+
+                for (let i = 1; i < this.hourDiff(previousHour,a.index); i++) {
+                const obj = { ...a };
+
+                    const newHour = (baseHour + i) % 24; 
+                    // console.log(newHour);
+                    obj.stamp = String(newHour).padStart(2, '0') + ':00';
+                    obj.index = obj.index.split('T')[0]+"T"+obj.hour;
+                    newArr.push(obj);
+                    previousHour = obj.index; 
+                }
+             }else{
+               previousHour = a.index; 
+             }
+
+            newArr.push(a);
+            
+        })
+
+        return newArr;
+       
+    }
+
+    getAveragePerHour(data, id) {
+
+        let averageByHour = data.reduce((acc, item) => {
+      
+
+        const hour = item.stamp.split('T')[1].split(':')[0]+":00";
+        const stamp = item.stamp.split('T')[0]+"T"+hour;
+        
+        // console.log(hour);
+        if (!acc[stamp]) {
+        acc[stamp] = { sensorId: id, stamp: `${hour}`, index: `${stamp}`, sum: 0, count: 0, value:0 };
+        }
+        
+        acc[stamp].sum += parseFloat(item.value);
+        acc[stamp].count += 1;
+        return acc;
+        }, {});
+   
+        // console.log(averageByHour);
+    
+        const arr = Object.keys(averageByHour)
+        .sort((a, b) => Number(a) - Number(b))
+        .map(key => {
+            const data = averageByHour[key]; 
+            return {
+            ...data,
+            hour: key,
+            value: (data.sum / data.count).toFixed(2)
+            };
+        });
+
+        return this.fillGaps(arr);
+
+    }
+
+    initChart(){
+        let dates = this.getDates();
+        // console.log(selectedType);
+        selectedSensors.forEach(s=>{
+            fetch(`https://skopje.pulse.eco/rest/dataRaw?sensorId=${s}&type=${selectedType}&&from=${dates.yesterday}&to=${dates.now}`)
+        .then(response => response.json())
+        .then(data => {
+                this.sensorData.push(this.getAveragePerHour(data, s));
+                // console.log(data);
+                // console.log(this.getAveragePerHour(data, s));
+                this.showChart();
+                this.setMinMax();
+        });
+        })
+        
+
+    }
+    
+    showChart(){
+        const series = this.getSeries();
+        const xLabels = this.getXlabels();
+        
+        // console.log(series);
+        // console.log(xLabels);
+        
+        options.series = series,
+        options.xaxis.categories = xLabels;
+        options.xaxis.range = 24;
+        this.chart.updateOptions(options);
+    }
+
+    getSeries(){
+
+        let arr = [];
+        selectedSensors.forEach(ss=>{
+
+            let obj={
+                name:"",
+                data: []
+            };
+
+            obj.name= sensorNames[ss];
+            obj.data = this.sensorData.flat(1).filter(s => s.sensorId == ss).map(d=>d["value"]);
+
+            // console.log(obj);
+            arr.push(obj);
+        })
+
+        return arr;
+    }
+    getXlabels(){
+        let arr = this.sensorData[0]
+        // .filter(s => s.sensorId == selectedSensors[0])
+        .map(d=>d["stamp"]);
+ 
+        return arr;
+    }
+    countUpFloat(el, start, end, duration = 700, decimals = 2) {
+        let startTime = null;
+
+        function animate(ts) {
+            if (!startTime) startTime = ts;
+            const p = Math.min((ts - startTime) / duration, 1);
+            const val = (p * (end - start) + start).toFixed(decimals);
+            el.textContent = val;
+
+            if (p < 1) requestAnimationFrame(animate);
+        }
+
+        requestAnimationFrame(animate);
+    }
+    setMinMax(){
+       let minElement =  document.getElementById("min");
+       let maxElement =  document.getElementById("max");
+       let types = [...document.getElementsByClassName("typeMinMax")];
+       let sMin = parseFloat(minElement.innerText);
+       let sMax = parseFloat(maxElement.innerText);
+       
+       let arr = this.sensorData.flat(1).map(m=>m.value).map(parseFloat);
+       let min = Math.min(...arr);
+       let max = Math.max(...arr);
+
+       types.forEach(t=>t.innerText=selectedType);
+
+
+       this.countUpFloat(minElement, sMin, min);
+       this.countUpFloat(maxElement, sMax, max);
+
+   
+    }
+       
+}
--- scripts/mainchart.js
+++ scripts/mainchart.js
@@ -4,12 +4,51 @@ let selectedSensors = [];
 let selectedType = 'pm10';
 let selectedYear = '2025';
 let selectedTimeframe = 'monthly';
+// options Treba da bide ovde za da moze da se pristapi od daily klasata
+const options = {
+        series: {},
+        chart: {
+            id: 'main-chart',
+            height: 350,
+            type: 'area',
+            background: 'transparent',
+            foreColor: '#98a2b3',
+            toolbar: {
+                show: true,
+                autoSelected: 'pan',
+                tools: { pan: true, zoom: true, reset: true, download: false }
+            }
+        },
+        colors: ['#ff4d4f', '#00e396', '#008ffb'],
+        stroke: { curve: 'smooth', width: 3 },
+        xaxis: {
+            categories: {},
+            range: {},
+            labels: {
+                rotate: -45,
+                style: { fontSize: '10px' },
+                trim: true
+            }
+        },
+        yaxis: {
+            // max:200,
+            min: 0,
+            labels: { formatter: (val) => val ? parseFloat(val).toFixed(2) : "0.00" }
+        },
+        tooltip: { theme: 'dark' },
+        dataLabels: { enabled: false },
+        legend: { position: 'top', labels: { colors: '#fff' } }
+    };
+
 
 const sensorNames = {
     "3568aa20-235a-408c-861b-279c9f4d7709": "Vodno",
     "1002": "MOEPP Miladinovci",
     "6c6a9ef6-85f9-45c0-9e01-0c9d2fb87bc2": "Capitol Mall",
-    "fef6bc74-bf86-4874-9531-51b033580379": "Taftalidze 2"
+    "fef6bc74-bf86-4874-9531-51b033580379": "Taftalidze 2",
+    "8defa36a-62ca-448a-9ffb-5a2848c2dfa0": "FINKI/ZOO",
+    "1000":"Centar",
+    "1003":"Karpos"
 };
 
 fetch('img/data/MOEPP-Miladinovci_Taftalidze 2_Capitol Mall_monthly_avg_pm.json')
@@ -39,14 +78,15 @@ function initDashboard() {
 }
 
 function setupFilters() {
+    // BITNO: NE TREBA DA SE MENUVA. AKO TREBA SAMO DA SE DODADAT RABOTI
+    [...document.querySelectorAll('input[name="timeframe"]')].forEach(t=>t.addEventListener('change', (e) => {
+        selectedTimeframe = e.target.value;
+        document.getElementById('yearSelection').style.display = (selectedTimeframe === 'monthly') ? 'flex' : 'none';
+        updateChart();
+    }));
+
     document.getElementById('yearSelect').addEventListener('change', (e) => {
         selectedYear = e.target.value;
-        document.getElementById('timeframeSection').style.display = (selectedYear === '2026') ? 'block' : 'none';
-        updateChart();
-    });
-
-    document.getElementById('timeframeSelect').addEventListener('change', (e) => {
-        selectedTimeframe = e.target.value;
         updateChart();
     });
 
@@ -73,18 +113,27 @@ function toggleSensor(sensorId, btn) {
     updateChart();
 }
 
+
 function updateChart() {
     let categories = [];
     let timeframeKey = "";
     let xRange = undefined;
     let startZoom = 0;
     let endZoom = 0;
-
-    if (selectedYear === '2026') {
+    let instance;
+    
         if (selectedTimeframe === 'daily') {
-            categories = Array.from({length: 31}, (_, i) => `Day ${i + 1}`);
-            timeframeKey = "Day";
-            xRange = 7;
+            // categories = Array.from({length: 31}, (_, i) => `Day ${i + 1}`);
+            // timeframeKey = "Day";
+            // xRange = 7;
+            
+            // timeframeKey = "Daily";
+
+            instance = new DailyData(chartInstance);
+            return;
+            categories = instance.getXlabels();
+            xRange=24;
+
         } else if (selectedTimeframe === 'weekly') {
             timeframeKey = "Week";
             xRange = 4; // Покажи 4 недели одеднаш
@@ -104,15 +153,13 @@ function updateChart() {
             endZoom = startZoom + 4;
 
         } else {
+
             categories = Array.from({length: 12}, (_, i) => `${selectedYear}-${String(i + 1).padStart(2, '0')}`);
             timeframeKey = "Month";
         }
-    } else {
-        categories = Array.from({length: 12}, (_, i) => `${selectedYear}-${String(i + 1).padStart(2, '0')}`);
-        timeframeKey = "Month";
-    }
 
     const allSeries = selectedSensors.map(sensorId => {
+        let lastValue=null;
         const chartData = categories.map((_, index) => {
             const timeValue = index + 1;
             const found = sensorData.find(d =>
@@ -126,47 +173,21 @@ function updateChart() {
             // Проверка за двата можни клучa: Value или MonthlyAvg
             if (found) {
                 const val = found.Value !== undefined ? found.Value : found.MonthlyAvg;
+                lastValue = val; 
                 return parseFloat(val).toFixed(2);
+                
             }
-            return null;
+            return lastValue; //ffill
         });
         return { name: sensorNames[sensorId] || sensorId, data: chartData };
     });
 
-    const options = {
-        series: allSeries,
-        chart: {
-            id: 'main-chart',
-            height: 350,
-            type: 'area',
-            background: 'transparent',
-            foreColor: '#98a2b3',
-            toolbar: {
-                show: true,
-                autoSelected: 'pan',
-                tools: { pan: true, zoom: true, reset: true, download: false }
-            }
-        },
-        colors: ['#ff4d4f', '#00e396', '#008ffb'],
-        stroke: { curve: 'smooth', width: 3 },
-        xaxis: {
-            categories: categories,
-            range: xRange,
-            labels: {
-                rotate: -45,
-                style: { fontSize: '10px' },
-                trim: true
-            }
-        },
-        yaxis: {
-            labels: { formatter: (val) => val ? parseFloat(val).toFixed(2) : "0.00" }
-        },
-        tooltip: { theme: 'dark' },
-        dataLabels: { enabled: false },
-        legend: { position: 'top', labels: { colors: '#fff' } }
-    };
+    options.series = allSeries;
+    options.xaxis.categories = categories;
+    options.xaxis.range = xRange;
 
     if (chartInstance) {
+        // chartInstance.updateOptions({series:options.series, xaxis: options.xaxis});
         chartInstance.updateOptions(options);
     } else {
         chartInstance = new ApexCharts(document.querySelector("#chart"), options);
